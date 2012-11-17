f9eaa55eacc6f7789ea8afed935ddca25e949834
Eisenberger Tamás <tamas@eisenberger.hu>
2012-11-17 10:57:35 +0100

    Extend the skeleton
    with some cmake rules and empty source files

---
+++ common/ONCommon-Tests/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argv, char** argc)
+{
+    std::cout << "Test" << std::endl;
+    exit(0);
+}
+
---
+++ server/ONServerCore/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argv, char** argc)
+{
+    std::cout << "ON Server Starting Up..." << std::endl;
+    exit(0);
+}
+
